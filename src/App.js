ffcbe0db76f98419ceb046d0d849ab2f5188861e
Mesekir19 <88715684+Mesekir19@users.noreply.github.com>
2022-11-24 23:15:23 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import Hero from "./components/Hero/Hero";
+import Post from './components/post/Post';
+function App() {
+  return (
+    <div className='App'>
+      <Hero></Hero>
+      <Post></Post>
+      
+    </div>
+  );
+}
+
+export default App;
